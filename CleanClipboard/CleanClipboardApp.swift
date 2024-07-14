4254c586091aab6634fc2c7a8a83d3e9e3aa2e45
Grzegorz Berk <grzegorzberk70@gmail.com>
2024-07-14 19:27:25 +0200

    Initial Commit

---
+++ CleanClipboard/CleanClipboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanClipboardApp.swift
+//  CleanClipboard
+//
+//  Created by Grzegorz Berk on 14/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct CleanClipboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
