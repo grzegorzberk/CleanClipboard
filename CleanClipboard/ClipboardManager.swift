902bcb995894281c072a59e72ae8861288c70235
Grzegorz Berk <grzegorzberk70@gmail.com>
2024-07-14 19:55:21 +0200

    inital commit

---
+++ CleanClipboard/ClipboardManager.swift
@@ -0,0 +1,8 @@
+//
+//  ClipboardManager.swift
+//  CleanClipboard
+//
+//  Created by Grzegorz Berk on 14/07/2024.
+//
+
+import Foundation
--- CleanClipboardTests/ClipboardManagerTests.swift
+++ CleanClipboardTests/ClipboardManagerTests.swift
@@ -6,9 +6,9 @@
 //
 
 import XCTest
-@testable import CleanClipboard
+@testable import PlainTextClipboard
 
-final class CleanClipboardTests: XCTestCase {
+class CleanClipboardTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
